24c041bdaf6ad718a8280aebabe59afd12490c4e
Pavel Ivanov <sudokey@gmail.com>
2019-05-02 20:39:43 +0300

    Fixed draggable

--- components/Editor/Cover/Image/index.jsx
+++ components/Editor/Cover/Image/index.jsx
@@ -1,75 +1,27 @@
-import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './styles.less';
 import IconCloseCircle from '../../../Icons/CloseCircle';
 
-const Image = ({
-  src, alt, index, onClickRemove, onChangeIndex,
-}) => {
-  const [dragStart, setDragStart] = useState(false);
-  const [dragActive, setDragActive] = useState(false);
-
-  return (
-    <div
-      draggable
-      className={classNames({
-        [styles.image]: true,
-        [styles.dragStart]: dragStart,
-        [styles.dragActive]: dragActive,
-      })}
-      onDragStart={(e) => {
-        setDragStart(true);
-        e.dataTransfer.setData('index', index);
-      }}
-      onDragEnter={() => {
-        setDragActive(true);
-      }}
-      onDragLeave={() => {
-        setDragActive(false);
-      }}
-      onDragOver={(e) => {
-        if (e) {
-          e.preventDefault();
-        }
-        e.dataTransfer.dropEffect = 'move';
-        return false;
-      }}
-      onDragEnd={() => {
-        setDragStart(false);
-      }}
-      onDrop={(e) => {
-        if (e) {
-          e.preventDefault();
-        }
-        const newIndex = +e.dataTransfer.getData('index');
-        if (!Number.isNaN(newIndex)) {
-          onChangeIndex(newIndex);
-        }
-        setDragActive(false);
-        return false;
-      }}
+const Image = ({ src, alt, onClickRemove }) => (
+  <div className={styles.image}>
+    <button
+      tabIndex="-1"
+      type="button"
+      className={styles.remove}
+      title="Удалить"
+      onClick={onClickRemove}
     >
-      <button
-        tabIndex="-1"
-        type="button"
-        className={styles.remove}
-        title="Удалить"
-        onClick={onClickRemove}
-      >
-        <IconCloseCircle />
-      </button>
-      <img src={src} alt={alt} />
-    </div>
-  );
-};
+      <IconCloseCircle />
+    </button>
+    <img src={src} alt={alt} />
+  </div>
+);
 
 Image.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string,
-  index: PropTypes.number.isRequired,
   onClickRemove: PropTypes.func.isRequired,
-  onChangeIndex: PropTypes.func.isRequired,
 };
 
 Image.defaultProps = {
--- components/Editor/Cover/index.jsx
+++ components/Editor/Cover/index.jsx
@@ -1,4 +1,6 @@
+import arrayMove from 'array-move';
 import { pick } from 'lodash';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './styles.less';
@@ -6,28 +8,30 @@ import FileInput from '../../FileInput';
 import IconPlus from '../../Icons/Plus';
 import Image from './Image';
 
-const Cover = ({ images, onChange }) => (
-  <div className={styles.cover}>
+const Cover = ({ images, onChange }) => {
+  const SortableImageItem = SortableElement(({ image, index }) => (
+    <div className={styles.item}>
+      <div className={styles.content}>
+        <Image
+          {...image}
+          onClickRemove={() => {
+            const result = [].concat(images);
+            result.splice(index, 1);
+            onChange(result);
+          }}
+        />
+      </div>
+    </div>
+  ));
+
+  const SortableImageList = SortableContainer(({ list }) => (
     <div className={styles.list}>
-      {images.map((image, index) => (
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <Image
-              {...image}
-              index={index}
-              onClickRemove={() => {
-                const result = [].concat(images);
-                result.splice(index, 1);
-                onChange(result);
-              }}
-              onChangeIndex={(newIndex) => {
-                const result = [].concat(images);
-                [result[index], result[newIndex]] = [result[newIndex], result[index]];
-                onChange(result);
-              }}
-            />
-          </div>
-        </div>
+      {list.map((image, index) => (
+        <SortableImageItem
+          key={index}
+          index={index}
+          image={image}
+        />
       ))}
       <div className={styles.item}>
         <div className={styles.content}>
@@ -41,7 +45,8 @@ const Cover = ({ images, onChange }) => (
                 const addedImages = Array.from(e.target.files).map(file => ({
                   src: URL.createObjectURL(file),
                 }));
-                onChange(images.concat(addedImages));
+                const result = images.concat(addedImages);
+                onChange(result);
               },
             }}
           >
@@ -57,8 +62,22 @@ const Cover = ({ images, onChange }) => (
         </div>
       </div>
     </div>
-  </div>
-);
+  ));
+
+  return (
+    <div className={styles.cover}>
+      <SortableImageList
+        axis="xy"
+        list={images}
+        shouldCancelStart={e => e.target.tagName === 'BUTTON'}
+        onSortEnd={({ oldIndex, newIndex }) => {
+          const result = arrayMove(images, oldIndex, newIndex);
+          onChange(result);
+        }}
+      />
+    </div>
+  );
+};
 
 Cover.propTypes = {
   images: PropTypes.arrayOf(PropTypes.shape(pick(Image.propTypes, 'src', 'alt'))),
